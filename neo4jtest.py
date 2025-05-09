155e8e048c2449f20770d47ff45f44b8bddd3e7c
Pablownski <sal24374@uvg.edu.gt>
2025-05-08 18:54:42 -0600

    reorganización de estructura

--- neo4jtest.py
+++ neo4jtest.py
@@ -2,27 +2,6 @@ from neo4j import GraphDatabase
 from envloader import NEO_USER, NEO_PASS, NEO_URI
 import colorama
 
-with GraphDatabase.driver(uri = NEO_URI, auth = (NEO_USER, NEO_PASS)) as driver:
-    try:
-        colorama.init(autoreset=True)
-        driver.verify_connectivity()
-        print(colorama.Fore.GREEN + "Neo4J connection successful")
-        driver.verify_authentication()
-        print(colorama.Fore.GREEN + "Neo4J authentication successful")
-    except Exception as e:
-        print(colorama.Fore.RED + f"Error: {e}")
-
-
-class Libro:    
-    def __init__(self, name, length, author, year, genres):  # length = n páginas; genres -> array[String] de géneros del libro
-        self.name = name;
-        self.length = length;
-        self.author = author;
-        self.year = year;
-        self.genres = genres;
-
-
-l1 = Libro("El Gran Diseño", 251, "Stephen Hawking", 2010, ["science", "physics", "philosophy"])
 
 '''
 Notas de las querys de Neo4J:
@@ -37,19 +16,39 @@ Para borrar, primero se hace un match y luego:
 DETACH DELETE *libro encontrado*
 '''
 
-
+class Libro:    
+    def __init__(self, name, length, author, year, genres):  # length = n páginas; genres -> array[String] de géneros del libro
+        self.name = name;
+        self.length = length;
+        self.author = author;
+        self.year = year;
+        self.genres = genres;
+        
+        
+        
 with GraphDatabase.driver(uri = NEO_URI, auth = (NEO_USER, NEO_PASS)) as driver:
-    driver.execute_query( 
-        """
-        CREATE (:Libro {name: $name, length: $length, author: $author, year: $year, genres: $genres})
-        """,
-        name=l1.name,
-        length=l1.length,
-        author=l1.author,
-        year=l1.year,
-        genres=l1.genres,
-        database_="neo4j"
+    try:
+        colorama.init(autoreset=True)
+        driver.verify_connectivity()
+        print(colorama.Fore.GREEN + "Neo4J connection successful")
+        driver.verify_authentication()
+        print(colorama.Fore.GREEN + "Neo4J authentication successful")
+        l1 = Libro("El Gran Diseño", 251, "Stephen Hawking", 2010, ["science", "physics", "philosophy"])
+        driver.execute_query( 
+            """
+            CREATE (:Libro {name: $name, length: $length, author: $author, year: $year, genres: $genres})
+            """,
+            name=l1.name,
+            length=l1.length,
+            author=l1.author,
+            year=l1.year,
+            genres=l1.genres,
+            database_="neo4j"
         )
-    print(colorama.Fore.GREEN + "Nodo de libro creado con éxito")
-    
+        print(colorama.Fore.GREEN + "Nodo de libro creado con éxito")
+    except Exception as e:
+        print(colorama.Fore.RED + f"Error: {e}")
+        
+
+
 
