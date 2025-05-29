0ec01cc86411c425bc84c35423e0dee376c5c342
Pablownski <sal24374@uvg.edu.gt>
2025-05-29 09:39:42 -0600

    Corrección de lógica en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,46 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Lobby from './pages/Lobby'
-import Buscar from './pages/Buscar'
-import Favoritos from './pages/Favoritos'
-import Descubrir from './pages/Descubrir'
-import Perfil from './pages/Perfil'
-import Login from './pages/Login'
-import Register from './pages/Register'
 import { useState } from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Buscar from './pages/Buscar'
+import Descubrir from './pages/Descubrir'
+import Favoritos from './pages/Favoritos'
+import Lobby from './pages/Lobby'
+import Login from './pages/Login'
+import Perfil from './pages/Perfil'
+import Register from './pages/Register'
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [usuario, setUsuario] = useState(null);
 
   // Si NO está logueado, solo mostrar Login y Register
-  if (!isLoggedIn) {
-    return (
-      <main className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
-        <Routes>
-          <Route path="/login" element={<Login onLogin={() => setIsLoggedIn(true)} />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="*" element={<Navigate to="/login" />} />
-        </Routes>
-      </main>
-    )
-  }
+  if (!usuario) {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
+      <Routes>
+        <Route path="/login" element={<Login onLogin={(user) => setUsuario(user)} />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </main>
+  );
+}
+
 
   // Si está logueado, mostrar el sitio completo
   return (
-    <div className="min-h-screen flex flex-col">
-      <Navbar />
-      <main className="flex-grow p-4">
-        <Routes>
-          <Route path="/" element={<Lobby />} />
-          <Route path="/buscar" element={<Buscar />} />
-          <Route path="/favoritos" element={<Favoritos />} />
-          <Route path="/descubrir" element={<Descubrir />} />
-          <Route path="/perfil" element={<Perfil />} />
-          <Route path="/login" element={<Navigate to="/" />} />
-        </Routes>
-      </main>
-    </div>
-  )
+  <div className='min-h-screen bg-gray-50'>
+    <Navbar usuario={usuario} setUsuario={setUsuario} />
+    <Routes>
+      <Route path="/" element={<Lobby />} />
+      <Route path="/buscar" element={<Buscar />} />
+      <Route path="/favoritos" element={<Favoritos />} />
+      <Route path="/descubrir" element={<Descubrir />} />
+      <Route path="/perfil" element={<Perfil />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  </div>
+);
+
 }
 
 export default App
